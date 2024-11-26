2f1c769be905eef978d5d582bc7efc8de054e62b
terrybjr <bkterryjr@gmail.com>
2024-11-25 20:29:45 -0600

    commit

--- web-app/angular/src/app/app.routes.ts
+++ web-app/angular/src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
     path: 'app1',
     loadComponent: () =>
       loadRemoteModule('app1', './Component').then((m) => m.AppComponent),
+  },
+  {
+    path: 'app2',
+    loadComponent: () =>
+      loadRemoteModule('app2', './Component').then((m) => m.AppComponent),
   }
 ];
