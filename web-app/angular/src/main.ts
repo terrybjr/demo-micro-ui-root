3af318ac4fbfba3345598fb172d745b49c8f63be
terrybjr <bkterryjr@gmail.com>
2024-11-26 20:06:07 -0600

    commit

--- web-app/angular/src/main.ts
+++ web-app/angular/src/main.ts
@@ -1,6 +1,8 @@
 import { initFederation } from '@angular-architects/native-federation';
 
-initFederation('federation.manifest.json')
+import { environment } from './environments/environment';
+
+initFederation(environment.manifestFile)
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
   .catch(err => console.error(err));
